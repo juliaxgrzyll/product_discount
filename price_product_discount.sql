0b78344acb3814d6263bc334d147a0243cb64d6a
juliaxgrzyll <julia.grzyll@gmail.com>
2025-04-28 11:42:06 +0200

    price_product_discount.sql

--- price_product_discount.sql
+++ price_product_discount.sql
@@ -1,7 +1,7 @@
 /*prices and discounts for benefits products from groups 23,45*/
 select
-	 mp.product_name              			          as product_name
-	,mp.product_price            		              as price_before_discount
+     mp.product_name              		      as product_name
+    ,mp.product_price            		      as price_before_discount
     ,round((mp.product_price*(1-pb.discount/100)),0)  as price_after_discount
     ,round(mp.product_price*(pb.discount/100),0)      as discount_value
 from my_products mp
